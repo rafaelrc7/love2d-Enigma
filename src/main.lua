86b489108da3ead0637e1d49ea1d2b8a1e781bcb
rafaelrc7 <rafaelrc7@gmail.com>
2020-07-13 04:12:01 -0300

    Added final features to GUI.

--- src/main.lua
+++ src/main.lua
@@ -2,11 +2,11 @@ local preset = require "rotorPresets";                      -- Imports
 local enigma = require "enigma";
 local tools = require "tools";
 
-local Width, Height, sx, sy = 1538, 864;                    -- WidthxHeight e escalas
+local Width, Height, sx, sy = 1728, 972;                    -- WidthxHeight e escalas
 
-local bg, btnLmpAtlas, btnQuad, lmpQuad, letterHighlight;   -- Objetos gráficos
+local bg, btnLmpAtlas, btnQuad, rtrCtrl, lmpQuad, letterHighlight;   -- Objetos gráficos
 local printFont, boardFont, boardFontBold;                  -- Fontes
-local lmpCoords, btnCoords, plgCoords = {}, {}, {};
+local lmpCoords, btnCoords, plgCoords, ctrlCoords = {}, {}, {}, {};
 local letterLayout = {                                      -- Layout do teclado (QWERTZ)
     {"Q", "W", "E", "R", "T", "Z", "U", "I", "O"},
     {"A", "S", "D", "F", "G", "H", "J", "K"},
@@ -15,7 +15,7 @@ local letterLayout = {                                      -- Layout do teclado
 local messageIn, messageOut = "", "";                       -- Input e output principais
 local rotorPositions, plugBoard = {};                       -- Posições dos rotores e estado do plugboard
 
-local lA, lB;                                               -- Letras de novas conexões do plugboard
+local chgRtrPos, lA, lB;                                               -- Letras de novas conexões do plugboard
 
 -- Criação do objeto Enigma
 local enMachine = enigma.create({preset.getPreset("I"), preset.getPreset("II"), preset.getPreset("III")}, preset.getPreset("B"));
@@ -46,7 +46,44 @@ local function drawRotors()         -- Desenha as posições dos rotores
     love.graphics.setColor(0, 0, 0);
 
     for i = 1, #rotorPositions do
-        love.graphics.printf(rotorPositions[i], sx*(1182 + 85*(i-1)), sy*434, sx*25, "center");
+        love.graphics.printf(rotorPositions[i][1], sx*(1182 + 85*(i-1)), sy*449, sx*25, "center");
+        love.graphics.printf(rotorPositions[i][2], sx*(1182 + 85*(i-1)), sy*(449 - 50), sx*25, "center");
+        love.graphics.printf(rotorPositions[i][3], sx*(1182 + 85*(i-1)), sy*(449 + 50), sx*25, "center");
+    end;
+
+end;
+
+local function stepRotorTemplate(rotorNum, step)
+    local presets = { "I", "II", "III", "IV", "V"};
+
+    if step == 1 then
+
+        if enigma.getRotor(enMachine, rotorNum) == "V" then
+            enigma.setRotor(enMachine, rotorNum, preset.getPreset("I"));
+            return;
+        end;
+
+        for k, v in ipairs(presets) do
+            if enigma.getRotor(enMachine, rotorNum) == v then
+                enigma.setRotor(enMachine, rotorNum, preset.getPreset(presets[k+1]));
+                return;
+            end;
+        end;
+
+    else
+
+        if enigma.getRotor(enMachine, rotorNum) == "I" then
+            enigma.setRotor(enMachine, rotorNum, preset.getPreset("V"));
+            return;
+        end;
+
+        for k, v in ipairs(presets) do
+            if enigma.getRotor(enMachine, rotorNum) == v then
+                enigma.setRotor(enMachine, rotorNum, preset.getPreset(presets[k-1]));
+                return;
+            end;
+        end;
+
     end;
 
 end;
@@ -59,6 +96,7 @@ function love.load ()
 
     bg = love.graphics.newImage("images/Enigma_PUC.png");
     btnLmpAtlas = love.graphics.newImage("images/thumbs_enigma.png");
+    rtrCtrl = love.graphics.newImage("images/botoes_rotor.png");
 
     sx = Width / bg:getWidth();     -- Calculo das escalas
     sy = Height / bg:getHeight();
@@ -72,7 +110,7 @@ function love.load ()
     lmpQuad = love.graphics.newQuad(19, 30, 46, 46, btnLmpAtlas:getDimensions());
     btnQuad = love.graphics.newQuad(112, 32, 47, 47, btnLmpAtlas:getDimensions());
 
-    -- Calculo das coordenadas de todos os botões, lampadas e conexões do plugboard de acordo com a escala
+    -- Cálculo das coordenadas de todos os botões, lampadas e conexões do plugboard de acordo com a escala
     local xo, yo = 884, 584;
     for lNum, line in ipairs(letterLayout) do
         local x;
@@ -86,6 +124,12 @@ function love.load ()
         yo = yo + 64;
     end;
 
+    xo, yo = 1179*sx, (398)*sy;
+    for i = 1, 3 do
+    	ctrlCoords[i] = { x=xo+(84*sx*i) - 48*sx, y=yo};
+    end;
+
+
 end;
 
 function love.update(dt)
@@ -93,7 +137,20 @@ function love.update(dt)
     -- Pega a posição dos rotores e conexões do plugboard mais atualizadas
 
     for i = 1, #enMachine.rotors do
-        rotorPositions[i] = enigma.getRotorPos(enMachine, i);
+		rotorPositions[i] = {};
+		rotorPositions[i][1] = enigma.getRotorPos(enMachine, i);
+
+		if rotorPositions[i][1] == "Z" then
+			rotorPositions[i][2] = "A";
+		else
+			rotorPositions[i][2] = string.char( string.byte(rotorPositions[i][1]) + 1 );
+		end;
+
+		if rotorPositions[i][1] == "A" then
+			rotorPositions[i][3] = "Z";
+		else
+			rotorPositions[i][3] = string.char( string.byte(rotorPositions[i][1]) - 1 );
+		end;
     end;
 
     plugBoard = enigma.getPlugboard(enMachine);
@@ -101,6 +158,12 @@ function love.update(dt)
 end;
 
 function love.textinput(t)
+
+    if chgRtrPos then
+        enigma.setOnePosition(enMachine, chgRtrPos, t:upper());
+        chgRtrPos = false;
+        return;
+    end;
 
     -- Recebe input do usuário e o codifica
 
@@ -124,37 +187,61 @@ function love.mousepressed( x, y, button )
 
     if button == 1 then
 
-        if not lA then                              -- Checa se uma primeira letra já foi selecionada
+    	if y < 564 and not lA then
+    		for num, ctrlCoord in ipairs(ctrlCoords) do
+                local d = math.sqrt( (x-(ctrlCoord.x + sx*24))^2 + (y-(ctrlCoord.y + sy*16))^2 );
 
-            for _, letter in pairs(plugBoard) do    -- Checa se o click foi feito numa posição válida do plugboard
-                local d = math.sqrt( (x-plgCoords[letter].x )^2 + (y-plgCoords[letter].y)^2 );
-
-                if d <= 25*sx  then
-                    lA = letter;
-                    break;
+                if d <= 16*sx then
+                    stepRotorTemplate(num, 1);
+                    return;
                 end;
 
-            end;
+                d = math.sqrt( (x-(ctrlCoord.x + sx*24))^2 + (y-(ctrlCoord.y + sy*111))^2 );
 
-        else
-
-            for _, letter in pairs(plugBoard) do    -- Caso uma primeira letra já foi selecionada, checa se o click foi valido para selecionar a segunda
-                local d = math.sqrt( (x-plgCoords[letter].x )^2 + (y-plgCoords[letter].y)^2 );
-
-                if d <= 25*sx  then
-                    lB = letter;
-                    break;
+                if d <= 16*sx then
+                    stepRotorTemplate(num, -1);
+                    return;
                 end;
 
-            end;
+                if x > (1179 + ((num-1)*85))*sx and x < (1209 + ((num-1)*85))*sx and y > 375*sy and y < 549*sy then
+                    chgRtrPos = num;
+                    return;
+                end;
 
-            if lB then                              -- Cria uma conexão do Plugboard caso duas letras válidas forem selecionadas e depois limpa as seleções
-                enigma.createPlug(enMachine, { {lA, lB} });
-            end;
+    		end;
+    	else
 
-            lA, lB = nil, nil
-        end;
+        	if not lA then                              -- Checa se uma primeira letra já foi selecionada
 
+            	for _, letter in pairs(plugBoard) do    -- Checa se o click foi feito numa posição válida do plugboard
+                	local d = math.sqrt( (x-plgCoords[letter].x )^2 + (y-plgCoords[letter].y)^2 );
+
+                	if d <= 25*sx  then
+                    	lA = letter;
+                    	break;
+                	end;
+
+            	end;
+
+        	else
+
+            	for _, letter in pairs(plugBoard) do    -- Caso uma primeira letra já foi selecionada, checa se o click foi valido para selecionar a segunda
+                	local d = math.sqrt( (x-plgCoords[letter].x )^2 + (y-plgCoords[letter].y)^2 );
+
+                	if d <= 25*sx  then
+                    	lB = letter;
+                    	break;
+                	end;
+
+            	end;
+
+            	if lB then                              -- Cria uma conexão do Plugboard caso duas letras válidas forem selecionadas e depois limpa as seleções
+                	enigma.createPlug(enMachine, { {lA, lB} });
+            	end;
+
+            	lA, lB = nil, nil
+        	end;
+		end;
     elseif lA then                                  -- Caso segundo click invalido, anula seleção
         lA, lB = nil, nil;
     end;
@@ -168,6 +255,12 @@ function love.draw ()
     drawRotors();
 
     love.graphics.setColor(1,1,1);
+
+    for i = 1, 3 do
+    	love.graphics.draw(rtrCtrl, ctrlCoords[i].x, ctrlCoords[i].y, 0, sx, sy);
+    	love.graphics.printf(enigma.getRotor(enMachine,i), ctrlCoords[i].x, ctrlCoords[i].y + sy*50, sx*50, "center");
+    end;
+
 
     if letterHighlight then         -- Caso existam letras selecionadas, desenha o highlight nas coordenadas
         love.graphics.draw(btnLmpAtlas, lmpQuad, lmpCoords[letterHighlight[2]].x, lmpCoords[letterHighlight[2]].y, 0, sx, sy, 0);
